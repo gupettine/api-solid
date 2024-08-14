f0211014b5ba3693ed9bef8350d95764f5e94ef1
gupettine <gupettine@gmail.com>
2024-08-14 14:05:23 -0300

    feature: validating check-in distance

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -19,8 +19,8 @@ describe('Check-in Use Case', () => {
       title: 'JavaScript Gym',
       description: '',
       phone: '',
-      latitude: new Decimal(0),
-      longitude: new Decimal(0),
+      latitude: new Decimal(-22.8949505),
+      longitude: new Decimal(-43.1260443),
     })
 
     vi.useFakeTimers()
@@ -81,5 +81,25 @@ describe('Check-in Use Case', () => {
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('Should not be able to check in on distant gym', async () => {
+    gymsRepository.items.push({
+      id: 'gym-02',
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(-22.9070535),
+      longitude: new Decimal(-43.1107213),
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-02',
+        userId: 'user-01',
+        userLatitude: -22.8949505,
+        userLongitude: -43.1260443,
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
