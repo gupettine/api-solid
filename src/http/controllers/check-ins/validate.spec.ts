629ff33af8105e153643b4fb2e299a3f641ed8bc
gupettine <gupettine@gmail.com>
2024-08-19 11:54:29 -0300

    feature: add rbac tests

--- src/http/controllers/check-ins/validate.spec.ts
+++ src/http/controllers/check-ins/validate.spec.ts
@@ -14,7 +14,7 @@
 //   })
 
 //   it('Should be able to validate a check-in', async () => {
-//     const { token } = await createAndAuthenticateUser(app)
+//     const { token } = await createAndAuthenticateUser(app, true)
 
 //     const user = await prisma.user.findFirstOrThrow()
 
